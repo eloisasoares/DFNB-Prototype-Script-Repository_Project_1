5fa2d3b667f27bc433feaf1fd3fe3ee7b2c84662
esoares2 <55720669+esoares2@users.noreply.github.com>
2019-10-25 17:06:31 -0600

    Add files via upload

--- Create View/dbo.v_balance.sql
+++ Create View/dbo.v_balance.sql
@@ -33,36 +33,36 @@ CREATE VIEW v_balance
 AS
      SELECT af.acct_id, 
             ad.acct_open_date AS loan_date, 
-            CONVERT (DECIMAL(10,0), ad.loan_amt) AS loan_amt,
+            CONVERT(DECIMAL(10, 0), ad.loan_amt) AS loan_amt, 
             af.as_of_date AS balance_date, 
             ROW_NUMBER() OVER(PARTITION BY af.acct_id, 
                                            ad.loan_amt
             ORDER BY af.acct_id, 
                      af.as_of_date, 
-                     af.cur_balance DESC) AS period,
-            CONVERT (DECIMAL(10,0), (CASE
-                WHEN LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
-                                             ORDER BY af.acct_id, 
-                                                      af.as_of_date, 
-                                                      af.cur_balance DESC) IS NULL
-                THEN ad.loan_amt
-                ELSE LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
-                ORDER BY af.acct_id, 
-                         af.as_of_date, 
-                         af.cur_balance DESC)
-            END)) AS previous_balance, 
-            CONVERT (DECIMAL(10,0), af.cur_balance - CASE
-                                 WHEN LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
-                             ORDER BY af.acct_id, 
-                                      af.as_of_date, 
-                                      af.cur_balance DESC) IS NULL
-                                 THEN ad.loan_amt
-                                 ELSE LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
-                                 ORDER BY af.acct_id, 
-                                          af.as_of_date, 
-                                          af.cur_balance DESC)
-                             END) AS payment, 
-            CONVERT (DECIMAL(10,0), af.cur_balance) AS current_balance,
+                     af.cur_balance DESC) AS period, 
+            CONVERT(DECIMAL(10, 0), (CASE
+                                         WHEN LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
+                                     ORDER BY af.acct_id, 
+                                              af.as_of_date, 
+                                              af.cur_balance DESC) IS NULL
+                                         THEN ad.loan_amt
+                                         ELSE LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
+                                         ORDER BY af.acct_id, 
+                                                  af.as_of_date, 
+                                                  af.cur_balance DESC)
+                                     END)) AS previous_balance, 
+            CONVERT(DECIMAL(10, 0), af.cur_balance - CASE
+                                                         WHEN LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
+                                                     ORDER BY af.acct_id, 
+                                                              af.as_of_date, 
+                                                              af.cur_balance DESC) IS NULL
+                                                         THEN ad.loan_amt
+                                                         ELSE LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
+                                                         ORDER BY af.acct_id, 
+                                                                  af.as_of_date, 
+                                                                  af.cur_balance DESC)
+                                                     END) AS payment, 
+            CONVERT(DECIMAL(10, 0), af.cur_balance) AS current_balance,
             CASE
                 WHEN CASE
                          WHEN LAG(af.cur_balance, 1) OVER(PARTITION BY af.acct_id
@@ -91,5 +91,4 @@ AS
                 ELSE 'NO PAYMENT IN THE PERIOD'
             END AS Payment_Status
      FROM dbo.t_account_fact AS af
-          JOIN dbo.t_account_dim AS ad ON ad.acct_id = af.acct_id
-     WHERE af.cur_balance <> '0.0000'
+          JOIN dbo.t_account_dim AS ad ON ad.acct_id = af.acct_id;
